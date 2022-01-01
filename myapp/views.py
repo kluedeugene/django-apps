4ae646e67b1d4e65e0e7b061b3949ebe63ef5f77
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-01-02 02:56:39 +0900

    7/14. 홈페이지 읽기기능 구현

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, HttpResponse
 import random
 
+{id:1, 'title':'routing','body':'Routing is..'}
+{id:2, 'title':'View','body':'View is..'}
+{id:3, 'title':'Model','body':'Model is..'}
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Random<h1>'+str(random.random()))
+    return HttpResponse('''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            <li>routing</li>
+            <li>view</li>
+            <li>model</li>
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
 
 def create(reauest):
     return HttpResponse('Create!')
