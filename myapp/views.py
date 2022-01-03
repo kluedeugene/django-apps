9e2ce73311a9326e6ea9f90a619d2b8c78142d7f
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-01-03 15:10:23 +0900

    전역변수데이터사용한 페이지

--- myapp/views.py
+++ myapp/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, HttpResponse
 import random
 
-{id:1, 'title':'routing','body':'Routing is..'}
-{id:2, 'title':'View','body':'View is..'}
-{id:3, 'title':'Model','body':'Model is..'}
-
+topics= [
+    {'id':1, 'title':'routing','body':'Routing is..'},
+    {'id':2, 'title':'View','body':'View is..'},
+    {'id':3, 'title':'Model','body':'Model is..'},
+    ]
 
 # Create your views here.
 def index(request):
-    return HttpResponse('''
+    global topics
+    ol= ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return HttpResponse(f'''
     <html>
     <body>
         <h1>Django</h1>
         <ol>
-            <li>routing</li>
-            <li>view</li>
-            <li>model</li>
+            {ol}
         </ol>
         <h2>Welcome</h2>
         Hello, Django
@@ -23,9 +26,9 @@ def index(request):
     </html>
     ''')
 
+def read(reauest, id):
+    return HttpResponse('Read!'+id)
+
 def create(reauest):
     return HttpResponse('Create!')
 
-
-def read(reauest, id):
-    return HttpResponse('Read!'+id)
