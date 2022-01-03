9c4fe5f9e518f4de7795d5e4000e904b73a5c7d5
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-01-03 15:24:58 +0900

    함수화

--- myapp/views.py
+++ myapp/views.py
@@ -7,27 +7,37 @@ topics= [
     {'id':3, 'title':'Model','body':'Model is..'},
     ]
 
-# Create your views here.
-def index(request):
+def HTMLTemplate(articleTag):
     global topics
     ol= ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
-    return HttpResponse(f'''
+    return f'''
     <html>
     <body>
-        <h1>Django</h1>
+        <h1><a href="/"> Django</a></h1>
         <ol>
             {ol}
         </ol>
-        <h2>Welcome</h2>
-        Hello, Django
+        {articleTag}
     </body>
     </html>
-    ''')
+    '''
+
+def index(request):
+    article= '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def read(reauest, id):
-    return HttpResponse('Read!'+id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article =f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
 
 def create(reauest):
     return HttpResponse('Create!')
